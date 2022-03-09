7e68296ad3c2ec0c081c0cdabb5c68fc4961c450
chandan_naresh <chandansutradhar2@gmail.com>
2022-03-09 12:47:25 +0530

    virtual DOM change demo

--- src/App.js
+++ src/App.js
@@ -1,31 +1,23 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { Login } from "./Login";
+import { useState } from "react";
 // import Login from "./Login";
 function App() {
-	var paraWelcomeMsg =
-		"Welcome to this site and this message is coming from server";
+	const [welcomeMsg, setWelcomeMsg] = useState(
+		"Welcome to this site and this message is coming from server",
+	);
 
-	const eventHandler = () => {
-		alert("image clicked event");
-	};
+	setInterval(() => {
+		setWelcomeMsg("Last Loggedin at" + Date.now().toString());
+	}, 3000);
+
 	return (
 		<div className="App">
 			<Login />
 			<header className="App-header">
 				<img src={logo} className="App-logo" alt="logo" />
-				<p>{paraWelcomeMsg}</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React With Typescript
-				</a>
-				<button onClick={eventHandler} className="button">
-					Click Me
-				</button>
+				<p>{welcomeMsg}</p>
 			</header>
 		</div>
 	);
