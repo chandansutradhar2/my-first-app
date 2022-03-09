3ae4028cb8b7c800a6c87c3ca74727b7231b0be9
chandan_naresh <chandansutradhar2@gmail.com>
2022-03-09 12:12:41 +0530

    intro to JSK

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
 import logo from "./logo.svg";
 import "./App.css";
-
+import { Login } from "./Login";
+// import Login from "./Login";
 function App() {
+	var paraWelcomeMsg =
+		"Welcome to this site and this message is coming from server";
+
+	const eventHandler = () => {
+		alert("image clicked event");
+	};
 	return (
 		<div className="App">
+			<Login />
 			<header className="App-header">
 				<img src={logo} className="App-logo" alt="logo" />
-				<p>Some new feature added</p>
+				<p>{paraWelcomeMsg}</p>
 				<a
 					className="App-link"
 					href="https://reactjs.org"
 					target="_blank"
 					rel="noopener noreferrer"
 				>
-					Learn React
+					Learn React With Typescript
 				</a>
+				<button onClick={eventHandler} className="button">
+					Click Me
+				</button>
 			</header>
 		</div>
 	);
---
+++ src/Login.js
@@ -0,0 +1,7 @@
+export function Login() {
+	return (
+		<div style={{ height: "300px", width: "auto", backgroundColor: "red" }}>
+			<h2>Login Component</h2>
+		</div>
+	);
+}
